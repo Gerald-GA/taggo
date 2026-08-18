7e39b1a0d68020b2b9b87c2a50b431b43391ed62
Gerald-GA <gerald.alanis@proton.me>
2026-08-18 16:41:00 -0600

    Added tag reading for each file in given directory.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Gerald-GA/taggo
 
 go 1.26.5
+
+require github.com/dhowden/tag v0.0.0-20240417053706-3d75831295e8 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/dhowden/tag"
 )
 
 func main() {
@@ -13,11 +15,11 @@ func main() {
 		return
 	}
 
-	album := os.Args[1]
-	fmt.Println("Fetching metadata for album: ", filepath.Base(album))
+	albumDir := os.Args[1]
+	fmt.Println("Fetching metadata for album: ", filepath.Base(albumDir))
 	fmt.Println("Scanning folder for audio files!")
 
-	files, err := os.ReadDir(album)
+	files, err := os.ReadDir(albumDir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,6 +27,25 @@ func main() {
 	for _, file := range files {
 		if !file.IsDir() {
 			fmt.Println("Found file:", file.Name())
+			f, err := os.Open(filepath.Join(albumDir, file.Name()))
+			if err != nil {
+				log.Fatalf("Failed to open file: %v", err)
+				return
+			}
+
+			m, err := tag.ReadFrom(f)
+			if err != nil {
+				log.Fatalf("Failed to parse tags: %v", err)
+				return
+			}
+
+			fmt.Println("Format:", m.Format()) // e.g., FLAC, MP4, MP3
+			fmt.Println("Title:", m.Title())
+			fmt.Println("Artist:", m.Artist())
+			fmt.Println("Album:", m.Album())
+			fmt.Println("Year:", m.Year())
+			f.Close()
+
 		}
 	}
 }
