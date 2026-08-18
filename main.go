edac5d66d28dcfadcd204f85d69dc17515babd29
Gerald-GA <gerald.alanis@proton.me>
2026-08-18 16:58:37 -0600

    Started using m.Raw() for reading a track's saved metadata, still need to sort or create a struct for metadata

--- main.go
+++ main.go
@@ -30,20 +30,17 @@ func main() {
 			f, err := os.Open(filepath.Join(albumDir, file.Name()))
 			if err != nil {
 				log.Fatalf("Failed to open file: %v", err)
-				return
 			}
 
 			m, err := tag.ReadFrom(f)
 			if err != nil {
 				log.Fatalf("Failed to parse tags: %v", err)
-				return
 			}
 
-			fmt.Println("Format:", m.Format()) // e.g., FLAC, MP4, MP3
-			fmt.Println("Title:", m.Title())
-			fmt.Println("Artist:", m.Artist())
-			fmt.Println("Album:", m.Album())
-			fmt.Println("Year:", m.Year())
+			for key, value := range m.Raw() {
+				fmt.Printf("%s: %s\n", key, value)
+			}
+			fmt.Println()
 			f.Close()
 
 		}
