8a60c8d7488a6ca9f7a23d5fb863574649027dbc
Gerald-GA <gerald.alanis@proton.me>
2026-08-19 12:39:22 -0600

    Added metadata search function

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Gerald-GA/taggo
 
 go 1.26.5
 
-require github.com/dhowden/tag v0.0.0-20240417053706-3d75831295e8 // indirect
+require (
+	github.com/dhowden/tag v0.0.0-20240417053706-3d75831295e8 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.15 // indirect
+)
--- main.go
+++ main.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 
 	"github.com/dhowden/tag"
 )
 
+type Parameters struct {
+	Artist  string
+	Album   string
+	Barcode string
+}
+
+type Metadata struct {
+}
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: deeztags <album folder>")
+		fmt.Println("Usage: taggo <album folder>")
 		return
 	}
 
@@ -24,10 +36,18 @@ func main() {
 		log.Fatal(err)
 	}
 
+	params := Parameters{
+		Artist:  "Kendo Kaponi",
+		Album:   "APOCALIPTO",
+		Barcode: "",
+	}
+	getMetadata(params)
+
 	for _, file := range files {
-		if !file.IsDir() {
+		filePath := filepath.Join(albumDir, file.Name())
+		if !file.IsDir() && filepath.Ext(file.Name()) == ".flac" {
 			fmt.Println("Found file:", file.Name())
-			f, err := os.Open(filepath.Join(albumDir, file.Name()))
+			f, err := os.Open(filePath)
 			if err != nil {
 				log.Fatalf("Failed to open file: %v", err)
 			}
@@ -37,12 +57,35 @@ func main() {
 				log.Fatalf("Failed to parse tags: %v", err)
 			}
 
+			// TODO: Sort map or create a new struct for holding metadata.
 			for key, value := range m.Raw() {
 				fmt.Printf("%s: %s\n", key, value)
 			}
 			fmt.Println()
 			f.Close()
-
 		}
 	}
+}
+
+func getMetadata(params Parameters) {
+	deezerURL := "https://api.deezer.com/"
+
+	if params.Barcode != "" {
+		deezerURL += "album/upc:" + params.Barcode
+	} else {
+		deezerURL += fmt.Sprintf("search/?q=artist:%qalbum:%q", url.QueryEscape(params.Artist), url.QueryEscape(params.Album))
+	}
+
+	fmt.Println(deezerURL)
+
+	resp, err := http.Get(deezerURL)
+	if err != nil {
+		log.Fatal("HTTP GET request failedd")
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Failed to read resp.Body")
+	}
+	fmt.Println(string(body))
 }
